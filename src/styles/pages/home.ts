2dde4143f140734f83e09631a1815a9057e5d2e5
Felipe Ribeiro <piporibeiro@gmail.com>
2022-09-30 21:27:07 -0300

    styles: add fontSizes

---
+++ src/styles/pages/home.ts
@@ -0,0 +1,56 @@
+import { styled } from '../index'
+
+export const HomeContainer = styled('main', {
+  display: 'flex',
+  width: '100%',
+  maxWidth: 'calc(100vw - ((100vw - 1180px) / 2))',
+  marginLeft: 'auto',
+  minHeight: 616,
+})
+
+export const Product = styled('a', {
+  background: 'linear-gradient(180deg, #1ea483 0%, #7465d4 100%)',
+  borderRadius: 8,
+  cursor: 'pointer',
+  position: 'relative',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  overflow: 'hidden',
+
+  img: {
+    objectFit: 'cover',
+  },
+
+  footer: {
+    position: 'absolute',
+    bottom: '0.25rem',
+    left: '0.25rem',
+    right: '0.25rem',
+    padding: '2rem',
+    borderRadius: 6,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    backgroundColor: 'rgba(0,0 ,0 , 0.6)',
+    transform: 'traslateY(100%)',
+    opacity: 0,
+    transition: 'all 0.2s ease-in-out',
+  },
+  strong: {
+    fontSize: '$lg',
+  },
+
+  span: {
+    fontSize: '$xl',
+    fontWeight: 'bold',
+    color: '$green300',
+  },
+
+  '&:hover': {
+    footer: {
+      transform: 'traslateY(0%)',
+      opacity: 1,
+    },
+  },
+})
