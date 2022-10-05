207b5623b6ffc71b145f5a3101d739e485234670
Felipe Ribeiro <piporibeiro@gmail.com>
2022-10-05 19:59:19 -0300

    feat: create product page

---
+++ src/pages/product/[id].tsx
@@ -0,0 +1,70 @@
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from '../../styles/pages/product'
+import { GetStaticPaths, GetStaticProps } from 'next'
+import { stripe } from '../../lib/stripe'
+import Stripe from 'stripe'
+import Image from 'next/future/image'
+
+interface ProductProps {
+  product: {
+    id: string
+    name: string
+    imageURL: string
+    price: string
+    description: string
+  }
+}
+
+export default function Product({ product }: ProductProps) {
+  return (
+    <ProductContainer>
+      <ImageContainer>
+        <Image src={product.imageURL} width={520} height={480} alt={''} />
+      </ImageContainer>
+      <ProductDetails>
+        <h1>{product.name}</h1>
+        <span>{product.price}</span>
+        <p>{product.description}</p>
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [{ params: { id: 'prod_MY2w1qS7KPHJbA' } }],
+    fallback: false,
+  }
+}
+
+export const getStaticProps: GetStaticProps<any, { id: string }> = async ({
+  params,
+}) => {
+  const productId = params.id
+
+  const product = await stripe.products.retrieve(productId, {
+    expand: ['default_price'],
+  })
+
+  const price = product.default_price as Stripe.Price
+
+  return {
+    props: {
+      product: {
+        id: product.id,
+        name: product.name,
+        imageURL: product.images[0],
+        price: new Intl.NumberFormat('pt-BR', {
+          style: 'currency',
+          currency: 'BRL',
+        }).format(price.unit_amount / 100),
+        description: product.description,
+      },
+    },
+    revalidate: 60 * 60 * 2, // 2 hours
+  }
+}
