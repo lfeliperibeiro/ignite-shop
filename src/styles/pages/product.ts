ffe8007750cdc4252c0bb9d8976b7d8fbf1f3e7c
Felipe Ribeiro <piporibeiro@gmail.com>
2022-10-14 19:45:09 -0300

    style: adjust css

--- src/styles/pages/product.ts
+++ src/styles/pages/product.ts
@@ -5,7 +5,6 @@ export const ProductContainer = styled('main', {
   gridTemplateColumns: '1fr 1fr',
   alignItems: 'stretch',
   gap: '4rem',
-  padding: '2rem 0',
   width: '100%',
   maxWidth: 1180,
   margin: '0 auto',
@@ -14,7 +13,7 @@ export const ProductContainer = styled('main', {
 export const ImageContainer = styled('div', {
   width: '100%',
   maxWidth: 576,
-  height: 656,
+  height: 616,
   background: 'linear-gradient(180deg, #1ea483 0%, #7465d4 100%)',
   borderRadius: 8,
   padding: '0.25rem',
@@ -61,7 +60,12 @@ export const ProductDetails = styled('div', {
     fontWeight: 'bold',
     fontSize: '$md',
 
-    '&:hover': {
+    '&:disabled': {
+      opacity: 0.6,
+      cursor: 'not-allowed',
+    },
+
+    '&:not(disabled):hover': {
       backgroundColor: '$green300',
     },
   },
