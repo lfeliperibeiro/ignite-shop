a5758513775fabca6a1a4f7f73ee462e1e2fb16e
Felipe Ribeiro <piporibeiro@gmail.com>
2022-10-14 20:14:30 -0300

    feat: create success page

---
+++ src/pages/success.tsx
@@ -0,0 +1,19 @@
+import { ImageContainer, SuccessContainer } from '../styles/pages/success'
+import Link from 'next/link'
+
+export default function SuccessPage() {
+  return (
+    <SuccessContainer>
+      <h1>Compra efetuada!</h1>
+      <ImageContainer></ImageContainer>
+
+      <p>
+        Uhuul <strong>Felipe Ribeiro</strong>, sua{' '}
+        <strong>Camiseta Beyond the Limits</strong> já está a caminho da sua
+        casa.
+      </p>
+
+      <Link href={'/'}>Voltar ao catálogo</Link>
+    </SuccessContainer>
+  )
+}
